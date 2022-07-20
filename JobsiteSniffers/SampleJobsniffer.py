4b36c18a1575fcde445296a21b7990038038fc04
Lia Neville <lianeville5@gmail.com>
2022-07-20 17:41:36 -0500

    Initial Structure

---
+++ JobsiteSniffers/SampleJobsniffer.py
@@ -0,0 +1,15 @@
+from faker import Faker
+fake = Faker()
+
+class SampleJobsniffer:
+	def __iter__(self):
+		self.a = 1
+		return self
+
+	def __next__(self):
+		if self.a <= 20:
+			x = self.a
+			self.a += 1
+			return x
+		else:
+			raise StopIteration
---
+++ main.py
@@ -0,0 +1,6 @@
+from JobsiteSniffers.SampleJobsniffer import SampleJobsniffer
+
+test = iter(SampleJobsniffer())
+
+for t in test:
+	print(t)
